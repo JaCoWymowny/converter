990a84a56daf90fe43cea628fa8c06d7ec511ec9
JaCoWymowny <jacekkombo@gmail.com>
2022-07-09 09:30:50 +0200

    first commit

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primaryColor: "#FF0000",
+    secondaryColor: "#00ff00",
+    detailColor: "#00FFFF",
+    white: "fff"
+  },
+
+  fontFamily: {
+    primaryFont: '"Roboto", "sans-serif"',
+  },
+}
+
+export default theme;
